da05c4d20ad15d76da0d96d1411fe3fc1d643755
Igor Krumpak <igorkrumpak@gmail.com>
2023-09-10 00:14:52 +0200

    pom update.

--- bear_bull_service/src/main/java/si/iitech/bear_bull_rest/AppResource.java
+++ bear_bull_service/src/main/java/si/iitech/bear_bull_rest/AppResource.java
@@ -12,27 +12,27 @@ import java.nio.file.Paths;
 @Path("/{path: (?!monitoring).+}")
 public class AppResource {
 
-    @GET
-    public Response serveApp(@PathParam("path") String path) {
+	@GET
+	public Response serveApp(@PathParam("path") String path) {
 
-        String resourcePath = "META-INF/resources/" + (path.isEmpty() ? "index.html" : path);
-        InputStream resourceAsStream = getClass().getClassLoader().getResourceAsStream(resourcePath);
+		String resourcePath = "META-INF/resources/" + (path.isEmpty() ? "index.html" : path);
+		InputStream resourceAsStream = getClass().getClassLoader().getResourceAsStream(resourcePath);
 
-        if (resourceAsStream != null) {
-            try {
-                String contentType = Files.probeContentType(Paths.get(resourcePath));
-                return Response.ok(resourceAsStream, contentType).build();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
+		if (resourceAsStream != null) {
+			try {
+				String contentType = Files.probeContentType(Paths.get(resourcePath));
+				return Response.ok(resourceAsStream, contentType).build();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
 
-        // If the asset is not found, serve the index.html for React to handle routing
-        InputStream indexHtml = getClass().getClassLoader().getResourceAsStream("META-INF/resources/index.html");
-        if (indexHtml != null) {
-            return Response.ok(indexHtml, "text/html").build();
-        }
+		// If the asset is not found, serve the index.html for React to handle routing
+		InputStream indexHtml = getClass().getClassLoader().getResourceAsStream("META-INF/resources/index.html");
+		if (indexHtml != null) {
+			return Response.ok(indexHtml, "text/html").build();
+		}
 
-        return Response.status(Response.Status.NOT_FOUND).build();
-    }
+		return Response.status(Response.Status.NOT_FOUND).build();
+	}
 }
